a4ab4272c843a0d1d3d8320d627c8ed83e3cef1f
ABDULRAZAQ79922 <111866517+ABDULRAZAQ79922@users.noreply.github.com>
2024-06-29 13:58:22 +0530

    completed

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("overlay-menu").style.width = "100%";
+});
+
+document.getElementById("close-menu").addEventListener("click", function() {
+    document.getElementById("overlay-menu").style.width = "0%";
+});
